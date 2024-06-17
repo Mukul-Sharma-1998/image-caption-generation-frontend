517830c8c4ed66b1a10c8ef3385b2a6643e96522
Mukul Sharma <60062787+Mukul-Sharma-1998@users.noreply.github.com>
2024-06-17 18:15:36 +0530

    updated main page

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -66,76 +66,84 @@ const ImageUpload = () => {
 
   return (
     <div>
-      <form encType='multipart/form-data' onSubmit={handleSubmit(onSubmit)} 
-      className='flex flex-col items-center justify-start sm:m-10 m-5'
-      >
-
-        <div className='m-0'>
-          <input 
-          className='absolute cursor-pointer w-[64px] h-[64px] opacity-0'
-          {...register('image', {
-            required: "Image is required",
-            onChange: handleImageUpload
-          })} type="file" accept='image/*' />
-          <img 
-          src={upload} 
-          alt="Upload Image"
-          className='w-full object-cover cursor-pointer animate-bounce'
-          onClick={() => document.querySelector('input[type="file"]').click()}
-          />
+      <div className='sm:flex justify-evenly'>
+        <div className='sm:flex items-center sm:w-1/3 hidden'>
+          <h1 className='text-yellow-300 text-center font-bold p-4 text-[40px] opacity-50'>Up your <br/> social media <br/> game!</h1>
         </div>
-        {errors.image && <div className='text-red-500'>{errors.image.message}</div>}
+        <form encType='multipart/form-data' onSubmit={handleSubmit(onSubmit)} 
+        className='flex flex-col items-center justify-start sm:m-10 m-5'
+        >
 
-       
-        <div className='m-4'>
-          <select {...register('platform', {
-            required: "Platform id required"
-          })}
-          onChange={(e) => {
-            setPlatform(e.target.value)
-          }}
-          className='bg-black text-yellow-300 rounded-sm border border-yellow-300 border-solid'
-          >
-            {platforms.map((eachPlatform, index) => (
-              <option value={eachPlatform} key={index}
-              className='w-32 h-8'
-              >
-                {eachPlatform}
-              </option>
-            ))}
-          </select>
+          <div className='m-0'>
+            <input 
+            className='absolute cursor-pointer w-[64px] h-[64px] opacity-0'
+            {...register('image', {
+              required: "Image is required",
+              onChange: handleImageUpload
+            })} type="file" accept='image/*' />
+            <img 
+            src={upload} 
+            alt="Upload Image"
+            className='w-full object-cover cursor-pointer animate-bounce'
+            onClick={() => document.querySelector('input[type="file"]').click()}
+            />
+          </div>
+          {errors.image && <div className='text-red-500'>{errors.image.message}</div>}
+
+        
+          <div className='m-4'>
+            <select {...register('platform', {
+              required: "Platform id required"
+            })}
+            onChange={(e) => {
+              setPlatform(e.target.value)
+            }}
+            className='bg-black text-yellow-300 rounded-sm border border-yellow-300 border-solid'
+            >
+              {platforms.map((eachPlatform, index) => (
+                <option value={eachPlatform} key={index}
+                className='w-32 h-8'
+                >
+                  {eachPlatform}
+                </option>
+              ))}
+            </select>
+          </div>
+          {errors.platform && <div className='text-red-500'>{errors.platform.message}</div>}
+
+        
+          <div className='m-4'>
+            <select {...register('mood', {
+              required: "Mood id required"
+            })}
+            onChange={(e) => {
+              setMood(e.target.value)
+            }}
+            className='bg-black text-yellow-300 rounded-sm border border-yellow-300 border-solid'
+            >
+              {moods.map((eachMood, index) => (
+                <option value={eachMood} key={index}>{eachMood}</option>
+              ))}
+            </select>
+          </div>
+          {errors.mood && <div className='text-red-500'>{errors.mood.message}</div>}
+
+          <div className='m-4 p-0'>
+            <button type='submit' disabled={isSubmitting} className={`text-black font-bold bg-yellow-300 rounded-xl p-3 ${isSubmitting && `animate-pulse`}`}>
+              {isSubmitting? "Generating..." : captions.length>0? "Generate Again" : "Generate"}
+            </button>
+          </div>
+
+          {errors.root && <div className='text-red-500'>{errors.root.message}</div>}
+        </form>
+        <div className='sm:flex items-center sm:w-1/3 hidden'>
+          <h1 className='text-yellow-300 text-center font-bold p-4 text-[40px] opacity-50'>Take it <br/> to the next level <br/> with the help of <br/> AI</h1>
         </div>
-        {errors.platform && <div className='text-red-500'>{errors.platform.message}</div>}
-
-      
-        <div className='m-4'>
-          <select {...register('mood', {
-            required: "Mood id required"
-          })}
-          onChange={(e) => {
-            setMood(e.target.value)
-          }}
-          className='bg-black text-yellow-300 rounded-sm border border-yellow-300 border-solid'
-          >
-            {moods.map((eachMood, index) => (
-              <option value={eachMood} key={index}>{eachMood}</option>
-            ))}
-          </select>
-        </div>
-        {errors.mood && <div className='text-red-500'>{errors.mood.message}</div>}
-
-        <div className='m-4'>
-          <button type='submit' disabled={isSubmitting} className={`text-black font-bold bg-yellow-300 rounded-xl w-20 h-10 ${isSubmitting && `animate-pulse`}`}>
-            {isSubmitting? "Loading..." : "Submit"}
-          </button>
-        </div>
-
-        {errors.root && <div className='text-red-500'>{errors.root.message}</div>}
-      </form>
+      </div>
 
       
       <div className='flex flex-col items-center'>
-        {imageUrl && <img src={imageUrl} alt='Uploaded Image' className='w-64 h-64'/>}
+        {imageUrl && <img src={imageUrl} alt='Uploaded Image' className='w-64 h-64 m-5 rounded-lg'/>}
         {captions.length>0 && <h1 className='text-yellow-300 font-bold mt-8 mb-2'>Customized Captions</h1>}
         {captions.map((caption, index) => (
           <div>
